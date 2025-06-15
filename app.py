124f2d1be1ca871f5adf233c2694afad86395467
rimaaidah <rimaaidah028@gmail.com>
2025-06-15 21:13:02 +0700

    ketiga

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def predict():
         return f"Terjadi error: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
